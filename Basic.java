b1ddc4b2cd0c734d54d037ce10459af4614c8f13
Shawon <shawonh986@gmail.com>
2024-03-07 00:14:32 +0800

    escape sequence

--- Basic.java
+++ Basic.java
@@ -1,6 +1,8 @@
 public class Basic {
     public static void main (String []args){
         System.out.println("shawon");
+        System.out.println("hello");
+        System.out.print("hello \n shawon \n 01305163829");
     }
     
 }
