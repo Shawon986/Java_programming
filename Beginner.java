a783b27da26325ea049cff22c51fb5275afc1a08
Shawon <shawonh986@gmail.com>
2024-03-10 02:06:54 +0800

    escape sequence

--- Beginner.java
+++ Beginner.java
@@ -1,6 +1,6 @@
 public class Beginner {
     public static void main(String[] args) {
-        System.out.println("Shawon \n Toufiq \n Alif");
+        System.out.println("Shawon \t \" Toufiq \" \t \\ Alif");
         
     }
 }
