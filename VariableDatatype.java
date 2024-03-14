000aa7def2b04ec82a0d1ad0ba194aefb5f57176
Shawon <shawonh986@gmail.com>
2024-03-14 22:27:27 +0800

    variable

--- VariableDatatype.java
+++ VariableDatatype.java
@@ -1,7 +1,10 @@
 public class VariableDatatype {
     public static void main(String[] args) {
         boolean b;
+        char c ;
+        c = 'S';
         b= true ;
         System.out.println(b);
+        System.out.println("c ="+c);
     }
 }
