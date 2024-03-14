68fc86517dd547ac169d507442bcba9767e63bd1
Shawon <shawonh986@gmail.com>
2024-03-14 23:58:08 +0800

    product details print

---
+++ Product.java
@@ -0,0 +1,17 @@
+public class Product {
+    public static void main(String[] args) {
+        int id = 101;
+        String title, description,category,price ;
+        price = "1985 euros";
+        title = "iphone15";
+        description = "perfect product with best image quality";
+        category = "phone";
+
+        System.out.println("id :"+id);
+        System.out.println("title :"+title);
+        System.out.println("price :"+price);
+        System.out.println("description :"+description);
+        System.out.println("category :"+category);
+
+    }
+}
