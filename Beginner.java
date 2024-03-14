6e11df01087ea427255be01ff8af009be49bcd69
Shawon <shawonh986@gmail.com>
2024-03-14 22:25:31 +0800

    dataType

--- Beginner.java
+++ Beginner.java
@@ -2,5 +2,15 @@ public class Beginner {
     public static void main(String[] args) {
         System.out.println("Shawon \t \" Toufiq \" \t \\ Alif");
         
+        double sum = 3.768;
+        int sum2 = 5;
+        short sum3 = 1234;
+        byte sum4 = 120;
+        long sum5 = 398437834;
+        float sum6 = 3.98f ;
+        boolean sum7 = false;
+        char sum8 = 'b';
+        
+
     }
 }
---
+++ VariableDatatype.java
@@ -0,0 +1,7 @@
+public class VariableDatatype {
+    public static void main(String[] args) {
+        boolean b;
+        b= true ;
+        System.out.println(b);
+    }
+}
