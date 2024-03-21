8e29f9a44bca30c747bac4ec4d5f144673c0aef6
Shawon <shawonh986@gmail.com>
2024-03-21 09:02:36 +0800

    start computing angle

---
+++ ComputeAngles.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class ComputeAngles {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter three ponts");
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
+        double x3 = input.nextDouble();
+        double y3 = input.nextDouble();
+    }
+}
--- Week4Mathematics.java
+++ Week4Mathematics.java
@@ -2,5 +2,6 @@ public class Week4Mathematics {
     public static void main(String[] args) {
         System.out.println(Math.sin(90));
         System.out.println(Math.cos(90));
+
     }
 }
