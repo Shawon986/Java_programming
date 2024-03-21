100843dec3a8c8f3e366aa3c32fe0c15a4cdb44b
Shawon <shawonh986@gmail.com>
2024-03-21 09:27:37 +0800

    length calculate

---
+++ StringMethod.java
@@ -0,0 +1,6 @@
+public class StringMethod {
+    public static void main(String[] args) {
+        String message = " Welcome to java class";
+        System.out.println("The length of" + message + " is " + message.length());
+    }
+}
