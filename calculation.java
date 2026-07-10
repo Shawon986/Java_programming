4eb4d09e1fec763a31b5ad7f3aa3901f424036ed
Shawon <shawonh986@gmail.com>
2026-07-10 22:20:38 +0800

    calculation

---
+++ calculation.java
@@ -0,0 +1,7 @@
+public class calculation {
+    public static void main(String[]args){
+        int a = 10, b = 13, c = 15;
+        int sum = a+b+c;
+        System.out.println("Sum = " + sum);
+    }
+}
