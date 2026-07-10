099fc96b39b7cf34badbbb97141a58997204dabb
Shawon <shawonh986@gmail.com>
2026-07-11 02:39:41 +0800

    operators and operand

--- calculation.java
+++ calculation.java
@@ -3,5 +3,27 @@ public class calculation {
         int a = 10, b = 13, c = 15;
         int sum = a+b+c;
         System.out.println("Sum = " + sum);
+
+        float division = (float) a / b;
+        System.out.println("Division = " + division);   
+
+        int minus = c - a ;
+        System.out.println("Minus = " + minus);
+
+        int multiply = a * b;
+        System.out.println("Multiply = " + multiply);
+
+        int modulo = c % a;
+        System.out.println("Modulo = " + modulo);
+
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a--);
+        System.out.println(a);
+        System.out.println(--a);
+
+        
     }
 }
